35edfd406240ad86d82e0076022d7bb63a380334
Gueg <e.viallet38@gmail.com>
2018-02-05 10:11:16 +0100

    UUIDs scan

--- app/src/main/java/com/gueg/rasp/BluetoothManager.java
+++ app/src/main/java/com/gueg/rasp/BluetoothManager.java
@@ -3,32 +3,38 @@ package com.gueg.rasp;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Handler;
+import android.os.ParcelUuid;
 import android.util.Log;
 import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.UUID;
 
 @SuppressWarnings({"WeakerAccess", "ResultOfMethodCallIgnored"})
 public class BluetoothManager {
+    
+    private static final String TAG = "BTManager";
     private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private MainActivity activity;
     private InputStream in;
     private OutputStream out;
-    private static final int CLOCK = 200;
-    private static final long MAX_DELAY = 5000L;
+    private static final long CLOCK = 200L;
     private static final String PI_MAC = "B8:27:EB:F0:82:5B";
-    private long lastSent = 0;
-    private long lastReceived = 0;
 
     BluetoothManager(MainActivity activity) {
         this.activity = activity;
-        initBluetooth();
+        if(bluetoothAdapter.isEnabled())
+            initBluetooth();
+        else
+            activity.startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 0);
     }
 
     private Handler bluetoothHandler = new Handler();
@@ -47,42 +53,64 @@ public class BluetoothManager {
                                 activity.setText(new SimpleDateFormat("HH':'mm':'ss",Locale.getDefault()).format(System.currentTimeMillis()) + " Reçu : " + string + "\n");
                             }
                         });
-                        lastReceived = System.currentTimeMillis();
                     }
                 }
             } catch(IOException e) {
                 e.printStackTrace();
             }
-            if(lastReceived>MAX_DELAY)
-
-            //send(y);
             bluetoothHandler.postDelayed(this, CLOCK);
         }
     };
 
 
     public void initBluetooth() {
-        if(!bluetoothAdapter.isEnabled())
-            activity.startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 0);
-
+        try {
+            UUID uuid = UUID.fromString("00001111-0000-1000-8000-00805f9b34fb");
+            Log.d(TAG,""+uuid);
+            BluetoothServerSocket server = bluetoothAdapter.listenUsingRfcommWithServiceRecord("Server", uuid);
+            BluetoothSocket socket = server.accept();
+            String str = "abcd";
+            socket.getOutputStream().write(str.getBytes());
+        } catch(IOException e) {
+            Log.e(TAG,"",e);
+        }
+        /*
         BluetoothDevice device = bluetoothAdapter.getRemoteDevice(PI_MAC);
-
+        BluetoothSocket socket = device.createRfcommSocketToServiceRecord()
+        ArrayList<BluetoothSocket> sockets = new ArrayList<>();
+        String[] usedUuids = new String[2];
         if(device!=null) {
+            for(ParcelUuid uuid : device.getUuids()) {
+                try {
+                    BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid.getUuid());
+                    socket.connect();
+                    if(socket.isConnected()) {
+                        sockets.add(socket);
+                        if(sockets.size()<=2)
+                            usedUuids[sockets.size()-1]=uuid.getUuid().toString();
+                        Log.d(TAG, "Socket creation succeeded with uuid " + uuid.getUuid().toString());
+                    }
+                } catch (IOException e) {
+                    Log.w(TAG, "Socket creation failed with uuid "+uuid.getUuid().toString());
+                }
+            }
+            Log.d(TAG,sockets.size()+" valid uuids found out of "+device.getUuids().length);
+            if(sockets.size()<2) {
+                Log.e(TAG,"Not enough UUIDs found.");
+                return;
+            }
             try {
-                BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(device.getUuids()[2].getUuid());
-                socket.connect();
-                in = socket.getInputStream();
-                out = socket.getOutputStream();
+                in = sockets.get(0).getInputStream();
+                out = sockets.get(1).getOutputStream();
                 bluetoothHandler.post(checkIncomingData);
-                Log.d(":-:", "Connected");
-                activity.connected();
+                activity.connected(usedUuids);
+                Toast.makeText(activity, "Connecté", Toast.LENGTH_SHORT).show();
             } catch(IOException e) {
-                Log.e(":-:", "Socket creation failed");
-                e.printStackTrace();
+                Log.e(TAG,"Failed to open io streams",e);
             }
         } else
             Toast.makeText(activity, "Raspberry non trouvé", Toast.LENGTH_SHORT).show();
-
+        */
     }
 
 
@@ -90,12 +118,15 @@ public class BluetoothManager {
     public void send(String cmd) {
         try {
             out.write(cmd.getBytes());
-            lastSent = System.currentTimeMillis();
+            Log.d(TAG,"Sending : "+cmd);
         } catch(IOException e) {
             e.printStackTrace();
         }
-        if(lastSent>MAX_DELAY)
-            Toast.makeText(activity, "-", Toast.LENGTH_SHORT).show();
     }
+
+    public boolean isConnected() {
+        return bluetoothAdapter.isEnabled();
+    }
+
 
 }
--- app/src/main/java/com/gueg/rasp/MainActivity.java
+++ app/src/main/java/com/gueg/rasp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.gueg.rasp;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +17,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int BLUETOOTH_PERMISSION = 0;
+
+    BroadcastReceiver bluetoothStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,BluetoothAdapter.ERROR)) {
+                case BluetoothAdapter.STATE_CONNECTED:
+                    if(manager!=null&&!manager.isConnected())
+                        manager.initBluetooth();
+                    break;
+                case BluetoothAdapter.STATE_DISCONNECTED:
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     BluetoothManager manager;
     TextView textView;
@@ -24,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener connectListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            manager.initBluetooth();
+            if(manager!=null)
+                manager.initBluetooth();
         }
     };
 
@@ -40,24 +64,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkPermission();
-
         interact = findViewById(R.id.btn);
         interact.setOnClickListener(connectListener);
         textView = findViewById(R.id.txt);
         editText = findViewById(R.id.send);
 
-        manager = new BluetoothManager(this);
+        registerReceiver(bluetoothStateChanged,new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+        checkPermission();
 
     }
 
-    public void connected() {
+    private void initBluetoothManager() {
+        manager = new BluetoothManager(this);
+    }
+
+    public void connected(String uuids[]) {
         interact.setOnClickListener(sendListener);
         interact.setText(R.string.btn_send);
+        textView.setText("in : "+uuids[0]+"\nout : "+uuids[1]);
     }
 
     public void setText(String text) {
-        textView.setText(text);
+        textView.append("\n"+text);
     }
 
     private void checkPermission() {
@@ -65,8 +93,27 @@ public class MainActivity extends AppCompatActivity {
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH))
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH}, BLUETOOTH_PERMISSION);
         } else
-            Log.d(":-:","Permission OK");
+            initBluetoothManager();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case BLUETOOTH_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initBluetoothManager();
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bluetoothStateChanged);
+    }
 
 }
